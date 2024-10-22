fb4f99c266685b6007b73418398d436b82febe47
Aurelia Melati Suci <116251043+aureliare@users.noreply.github.com>
2024-10-23 01:44:11 +0700

    upload mvc

---
+++ mvc/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb2024_a_223040045_pw/mvc/public');
+
+define ('DB_HOST', 'localhost');
+define ('DB_USER', 'root');
+define ('DB_PASS', '');
+define ('DB_NAME','phpmvc');
+
